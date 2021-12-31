c8a7aa7fd6964a3194faf1bc6f858729a054c648
Muhammad Raihan Suryanom <raihansuryanom@gmail.com>
2021-12-31 11:16:26 +0700

    add support for react native web

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function (api) {
   api.cache(true);
+
+  const presets = ['babel-preset-expo'];
+  const plugins = [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '^react-native$': 'react-native-web',
+        },
+      },
+    ],
+  ];
+
   return {
-    presets: ['babel-preset-expo'],
+    presets,
+    plugins,
   };
 };
